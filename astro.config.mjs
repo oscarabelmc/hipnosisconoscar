a29632b91e44e9ed0c7181d48d8def57900fdc4b
Churritzu <churritzu@yahoo.com>
2026-01-29 18:15:42 -0800

    config: add tailwind & zorro12 host in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  server: {
+      allowedHosts: ['hipnosisconoscar.com', 'zorro12.local'],
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve(fileURLToPath(import.meta.url), '../src'),
+      },
+    },
+  },
+});
