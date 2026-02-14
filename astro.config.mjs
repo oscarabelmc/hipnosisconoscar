47fd73b8799f56e1ed927e5b64a6b200af7c484a
Churritzu <churritzu@yahoo.com>
2026-02-14 12:44:24 -0800

    deps: add react integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,22 +5,27 @@ import path from 'node:path';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-    i18n: {
-        locales: ['es', 'en'],
-        defaultLocale: 'es',
-    },
-    server: {
-        allowedHosts: ['hipnosisconoscar.com', 'zorro12.local'],
-    },
+  i18n: {
+      locales: ['es', 'en'],
+      defaultLocale: 'es',
+  },
 
-    vite: {
-        plugins: [tailwindcss()],
-        resolve: {
-            alias: {
-                '@': path.resolve(fileURLToPath(import.meta.url), '../src'),
-            },
-        },
-    },
+  server: {
+      allowedHosts: ['hipnosisconoscar.com', 'zorro12.local'],
+  },
+
+  vite: {
+      plugins: [tailwindcss()],
+      resolve: {
+          alias: {
+              '@': path.resolve(fileURLToPath(import.meta.url), '../src'),
+          },
+      },
+  },
+
+  integrations: [react()],
 });
